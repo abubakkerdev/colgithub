604b83481a22fabd23f55e053533a55bf59575db
rayan2228 <rhrayan2228@gmail.com>
2024-02-27 10:55:25 +0600

    rayan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const handleClick = () => {
+    setShow(!show);
+  };
+
   return (
     <div>
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-      <h2>Change Title or Toggle</h2>
+      {show && <h2 className="web">Change Title or Toggle</h2>}
+
+      <br />
+      <button onClick={handleClick}>Click Here</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
